9696fbde8de6530f04ade1f07c108b85ac343ea4
rdvnburton-lab <ridvanhsyn@gmail.com>
2026-01-03 02:28:40 +0300

    migration hatası giderildi

---
+++ backend/IstasyonDemo.Api/Migrations/20260102232740_UpdateVardiyaWithSorumluAndFileFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IstasyonDemo.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVardiyaWithSorumluAndFileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SorumluAdi",
+                table: "Vardiyalar",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SorumluId",
+                table: "Vardiyalar",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SorumluAdi",
+                table: "Vardiyalar");
+
+            migrationBuilder.DropColumn(
+                name: "SorumluId",
+                table: "Vardiyalar");
+        }
+    }
+}
